75597125260919227672ba1bafebceb245475289
Kirbuchi <kirbuchi@kirbuchi.com>
2018-02-07 18:39:01 -0500

    Return CORS headers for risk-types endpoint

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_cors import CORS
 
 
 def create_app(config_name=None):
@@ -8,6 +9,7 @@ def create_app(config_name=None):
     from api.views import api_views
 
     app = Flask(__name__)
+    CORS(app)
 
     if config_name:
         config_path = 'api.settings.{}'.format(config_name)
